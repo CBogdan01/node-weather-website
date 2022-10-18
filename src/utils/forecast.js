e68b4f39c9c6cf3f257f47502a1a50e4c6640ee4
Bogdan <bogdan.cotoara01@gmail.com>
2022-10-18 11:40:25 +0300

    Add chance of rain

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,22 @@
 const request = require("postman-request");
 
 const forecast = (latitude, longitude, callback) => {
-    const weatherAPI = `http://api.weatherstack.com/current?access_key=4082054d35b1e65cc7b2b142dcaef17d&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`
+  const weatherAPI = `http://api.weatherstack.com/current?access_key=4082054d35b1e65cc7b2b142dcaef17d&query=${encodeURIComponent(
+    latitude
+  )},${encodeURIComponent(longitude)}`;
 
-    request(weatherAPI, {json: true}, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location!', undefined)
-        } else {
-            callback(undefined,
-                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
-            )
-        }
-    })
-}
+  request(weatherAPI, { json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location!", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. There is a ${body.current.precip}% chance of rain.`
+      );
+    }
+  });
+};
 
-
-module.exports = forecast
+module.exports = forecast;
